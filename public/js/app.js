41278bf4fc370197068fb4a07cdc0a4cb5cb143d
romulobr <romulobr@gmail.com>
2014-07-20 17:08:23 -0400

    adds angular-js and ng-route

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,46 @@
-{
-	"directory": "public/components"
-}
+(function() {
+	var app = angular.module('pr2014', ['ngRoute']);
+	var partials = {
+		home: '/js/partials/home.html',
+		achievements: '/js/partials/achievements.html',
+		feedbacks: '/js/partials/feedbacks.html',
+		skills: '/js/partials/skills.html'
+	};
+
+	app.config(['$routeProvider',
+	  function($routeProvider) {
+	    $routeProvider.
+	      when('/', {
+	        templateUrl: partials.home
+	      }).
+	      when('/skills', {
+	        templateUrl: partials.skills
+	      }).
+	      when('/achievements', {
+	        templateUrl: partials.achievements
+	      }).	      
+	      when('/feedbacks', {
+	        templateUrl: partials.feedbacks
+	      }).
+	      otherwise({
+	        redirectTo: '/'
+	      });
+	  }]);
+
+	var menuItems = [{
+		title: 'Skills',
+		route: '/skills'
+	},
+	{
+		title: 'Achievements',
+		route: '/achievements'
+	},
+	{
+		title: 'Feedbacks',
+		route: '/feedbacks'
+	}];
+
+	app.controller('NavigationController', function (){
+		this.menuItems = menuItems;
+	});
+})();
