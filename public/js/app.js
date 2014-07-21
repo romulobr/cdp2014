cdab9c0fe8517a21ec538e15e887484415c332db
romulobr <romulobr@gmail.com>
2014-07-21 00:41:29 -0400

    adds some formatting to skills page.

--- public/js/app.js
+++ public/js/app.js
@@ -46,7 +46,7 @@
 		var values = {
 			low : 25,
 			'very low': 10,
-			high: 70,
+			high: 80,
 			'very high': 90,
 			medium: 50
 		};
@@ -61,37 +61,38 @@
 	};
 
 	var skills = [
-		createSkillImprovement('Coding','Android','P2', 'low', 'medium'),
-		createSkillImprovement('Coding','Java','P2', 'high', 'very high'),
-		createSkillImprovement('Coding','Ruby','P2', 'medium', 'high'),
-		createSkillImprovement('Coding','C#','P2', 'low', 'medium'),
-		createSkillImprovement('Coding','HTML5','P2', 'low', 'medium'),
-		createSkillImprovement('Coding','Javascript','P2', 'medium', 'high'),
-		createSkillImprovement('Coding','Mobile Web','P2', 'low', 'high'),
-		createSkillImprovement('Coding','NoSQL','P2', 'medium', 'high'),
-		createSkillImprovement('Coding','Android','P2', 'low', 'medium'),
-		createSkillImprovement('Coding','Jenkins','P2', 'low', 'medium'),
+		createSkillImprovement('coding','Android','P2', 'low', 'medium'),
+		createSkillImprovement('coding','Java','P2', 'high', 'very high'),
+		createSkillImprovement('coding','Ruby','P2', 'medium', 'high'),
+		createSkillImprovement('coding','C#','P2', 'low', 'medium'),
+		createSkillImprovement('coding','HTML5','P2', 'low', 'medium'),
+		createSkillImprovement('coding','Javascript','P2', 'medium', 'high'),
+		createSkillImprovement('coding','Mobile Web','P2', 'low', 'high'),
+		createSkillImprovement('coding','NoSQL','P2', 'medium', 'high'),
+		createSkillImprovement('coding','Android','P2', 'low', 'medium'),
+		createSkillImprovement('coding','Jenkins','P2', 'low', 'medium'),
 
-		createSkillImprovement('Testing','Capybara','P2', 'none', 'low'),
-		createSkillImprovement('Testing','Web Driver','P2', 'low', 'medium'),
-		createSkillImprovement('Testing','Testing strategies','P2', 'medium', 'high'),
+		createSkillImprovement('testing','Capybara','P2', 'none', 'low'),
+		createSkillImprovement('testing','Web Driver','P2', 'low', 'medium'),
+		createSkillImprovement('testing','Testing strategies','P2', 'medium', 'high'),
 		
-		createSkillImprovement('Soft Skill','Communication','P2', 'low', 'medium'),
-		createSkillImprovement('Soft Skill','Facilitation','P2', 'low', 'medium'),
-		createSkillImprovement('Soft Skill','Influence','P2', 'low', 'medium'),
-		createSkillImprovement('Soft Skill','Negotiation','P2', 'low', 'medium'),
-		createSkillImprovement('Soft Skill','Planning','P2', 'low', 'medium'),
-		createSkillImprovement('Soft Skill','Presenting','P2', 'low', 'medium'),
-		createSkillImprovement('Soft Skill','Problem Solving','P2', 'low', 'medium'),
-		createSkillImprovement('Soft Skill','Questioning','P2', 'low', 'medium'),
-		createSkillImprovement('Soft Skill','Synthesis','P2', 'low', 'medium'),
-		createSkillImprovement('Soft Skill','Teaching','P2', 'low', 'medium'),
+		createSkillImprovement('soft','Communication','P2', 'low', 'medium'),
+		createSkillImprovement('soft','Facilitation','P2', 'low', 'medium'),
+		createSkillImprovement('soft','Influence','P2', 'low', 'medium'),
+		createSkillImprovement('soft','Negotiation','P2', 'low', 'medium'),
+		createSkillImprovement('soft','Planning','P2', 'low', 'medium'),
+		createSkillImprovement('soft','Presenting','P2', 'low', 'medium'),
+		createSkillImprovement('soft','Problem Solving','P2', 'low', 'medium'),
+		createSkillImprovement('soft','Questioning','P2', 'low', 'medium'),
+		createSkillImprovement('soft','Synthesis','P2', 'low', 'medium'),
+		createSkillImprovement('soft','Teaching','P2', 'low', 'medium'),
+		createSkillImprovement('soft','Feedback & Coaching','P2', 'low', 'high'),
 
-		createSkillImprovement('UX','UI Development','P2', 'very-low', 'medium'),
-		createSkillImprovement('UX','Usability Research','P2', 'very-low', 'medium'),
-		createSkillImprovement('UX','Usability Testing','P2', 'very-low', 'medium'),
-		createSkillImprovement('UX','Visual Design','P2', 'very-low', 'low'),
-		createSkillImprovement('UX','Feedback & Coaching','P2', 'low', 'high')
+		createSkillImprovement('ux','UI Development','P2', 'very-low', 'medium'),
+		createSkillImprovement('ux','Usability Research','P2', 'very-low', 'medium'),
+		createSkillImprovement('ux','Usability Testing','P2', 'very-low', 'medium'),
+		createSkillImprovement('ux','Visual Design','P2', 'very-low', 'low'),
+		
 	];
 
 	app.controller('NavigationController', function (){
