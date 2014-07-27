c49c5e29feb861ff68e02ca9ec3b58939450b5c5
romulobr <romulobr@gmail.com>
2014-07-27 19:59:14 -0400

    extracts skills into it's own module.

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 (function() {
-	var app = angular.module('pr2014', ['ngRoute']);
+	var app = angular.module('pr2014', ['ngRoute','skills']);
 	var partials = {
 		home: '/js/partials/home.html',
 		achievements: '/js/partials/achievements.html',
@@ -40,62 +40,9 @@
 		title: 'Feedbacks',
 		route: '/feedbacks'
 	}];
-
-
-	var createSkillImprovement = function(category, technology, classification, before, after) {
-		var values = {
-			low : 25,
-			'very low': 10,
-			high: 80,
-			'very high': 90,
-			medium: 50
-		};
-		
-		return {
-			category: category,
-			technology: technology,
-			classification: classification,
-			before: values[before],
-			after: values[after]
-		};
-	};
-
-	var skills = [
-		createSkillImprovement('coding','Android','P2', 'low', 'medium'),
-		createSkillImprovement('coding','Java','P2', 'high', 'very high'),
-		createSkillImprovement('coding','Ruby','P2', 'medium', 'high'),
-		createSkillImprovement('coding','C#','P2', 'low', 'medium'),
-		createSkillImprovement('coding','HTML5','P2', 'low', 'medium'),
-		createSkillImprovement('coding','Javascript','P2', 'medium', 'high'),
-		createSkillImprovement('coding','Mobile Web','P2', 'low', 'high'),
-		createSkillImprovement('coding','NoSQL','P2', 'medium', 'high'),
-		createSkillImprovement('coding','Android','P2', 'low', 'medium'),
-		createSkillImprovement('coding','Jenkins','P2', 'low', 'medium'),
-
-		createSkillImprovement('testing','Capybara','P2', 'none', 'low'),
-		createSkillImprovement('testing','Web Driver','P2', 'low', 'medium'),
-		createSkillImprovement('testing','Testing strategies','P2', 'medium', 'high'),
-		
-		createSkillImprovement('soft','Communication','P2', 'low', 'medium'),
-		createSkillImprovement('soft','Presenting','P2', 'low', 'medium'),
-		createSkillImprovement('soft','Problem Solving','P2', 'low', 'medium'),
-		createSkillImprovement('soft','Questioning','P2', 'low', 'medium'),
-		createSkillImprovement('soft','Synthesis','P2', 'low', 'medium'),
-		createSkillImprovement('soft','Teaching','P2', 'low', 'medium'),
-		createSkillImprovement('soft','Feedback & Coaching','P2', 'low', 'high'),
-
-		createSkillImprovement('ux','UI Development','P2', 'very-low', 'medium'),
-		createSkillImprovement('ux','Usability Research','P2', 'very-low', 'medium'),
-		createSkillImprovement('ux','Usability Testing','P2', 'very-low', 'medium'),
-		createSkillImprovement('ux','Visual Design','P2', 'very-low', 'low'),
-		
-	];
-
+	
 	app.controller('NavigationController', function (){
 		this.menuItems = menuItems;
 	});
 
-	app.controller('SkillController', function(){
-		this.skills = skills;
-	});
 })();
---
+++ public/js/skills/skillsController.js
@@ -0,0 +1,58 @@
+(function() {
+	var app = angular.module('skills', []);	
+	
+	var createSkillImprovement = function(category, technology, classification, before, after) {
+		var values = {
+			'very low': 10,
+			low : 25,
+			medium: 50,
+			high: 80,
+			'very high': 90			
+		};
+		
+		return {
+			category: category,
+			technology: technology,
+			classification: classification,
+			before: values[before],
+			after: values[after]
+		};
+	};
+
+	var skills = [
+		createSkillImprovement('coding','Android','P2', 'low', 'medium'),
+		createSkillImprovement('coding','Java','P2', 'high', 'very high'),
+		createSkillImprovement('coding','Ruby','P2', 'medium', 'high'),
+		createSkillImprovement('coding','C#','P2', 'low', 'medium'),
+		createSkillImprovement('coding','HTML5 & Mobile Web','P2', 'low', 'medium'),
+		createSkillImprovement('coding','Javascript','P2', 'medium', 'high'),
+		createSkillImprovement('coding','NoSQL','P2', 'medium', 'high'),
+		createSkillImprovement('coding','Android','P2', 'low', 'medium'),
+		createSkillImprovement('coding','Jenkins','P2', 'low', 'medium'),
+
+		createSkillImprovement('testing','Capybara','P2', 'none', 'low'),
+		createSkillImprovement('testing','Web Driver','P2', 'low', 'medium'),
+		createSkillImprovement('testing','Testing strategies','P2', 'medium', 'high'),
+		
+		createSkillImprovement('soft','Communication','P2', 'medium', 'very high'),
+		createSkillImprovement('soft','Presenting','P2', 'low', 'high'),
+		createSkillImprovement('soft','Problem Solving','P2', 'low', 'medium'),
+		createSkillImprovement('soft','Synthesis','P2', 'low', 'medium'),
+		createSkillImprovement('soft','Teaching','P2', 'medium', 'very high'),
+		createSkillImprovement('soft','Feedback & Coaching','P2', 'low', 'very high'),
+
+		createSkillImprovement('ux','UI Development','P2', 'very-low', 'medium'),
+		createSkillImprovement('ux','Usability Research','P2', 'very-low', 'medium'),
+		createSkillImprovement('ux','Usability Testing','P2', 'very-low', 'medium'),
+		createSkillImprovement('ux','Visual Design','P2', 'very-low', 'low'),
+
+		createSkillImprovement('language','Portuguese','P2', 'high', 'high'),
+		createSkillImprovement('language','English','P2', 'high', 'very-high'),
+		createSkillImprovement('language','Spanish','P2', 'very-low', 'medium')
+		
+	];
+
+	app.controller('SkillController', function(){
+		this.skills = skills;
+	});
+})();
